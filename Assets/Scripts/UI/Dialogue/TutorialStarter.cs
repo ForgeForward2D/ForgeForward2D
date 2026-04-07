649807aee186a6f32ed42e02dbb659d27db14ccd
Nils Ettner <nils.ettner@web.de>
2026-04-07 18:08:55 +0200

    remove comments

--- Assets/Scripts/UI/Dialogue/TutorialStarter.cs
+++ Assets/Scripts/UI/Dialogue/TutorialStarter.cs
@@ -7,9 +7,7 @@ public class TutorialStarter : MonoBehaviour
 
     private IEnumerator Start()
     {
-        // Wait until UIManager is ready (UI scene may still be loading)
         yield return new WaitUntil(() => FindAnyObjectByType<UIManager>() != null);
-        // One extra frame so all Start() methods across scenes have run
         yield return null;
         npcController.HandleInteraction(UIPage.None);
     }
